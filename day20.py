7aee17347964a8d4768c9218e5db4d2c7a49fbde
Thomas Funk <thomasfunk10@gmail.com>
2016-11-19 18:42:13 -0600

    Day 20 Part 1
    Once I realized I was just looking for factors I cheated and stole a
    function off stack exchange.

---
+++ day20.py
@@ -0,0 +1,26 @@
+houses = 100000
+max = 34000000
+gifts = 0
+'''
+while gifts < max:
+	houses += 1
+	#for house in range(1,houses):
+	gifts = 0
+	for elf in range(1,houses):	
+		if houses % elf == 0:
+			gifts += elf*10
+'''
+from math import sqrt
+def factors(n):    
+    result = set()
+    for i in range(1, int(n ** 0.5) + 1):
+        div, mod = divmod(n, i)
+        if mod == 0:
+            result |= {i, div}
+    return result
+	
+while gifts < max:
+	houses += 1
+	gifts = 10 * sum(factors(houses))
+
+print('House # %d gets %d gifts' % (houses, gifts))
